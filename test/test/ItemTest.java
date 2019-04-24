d420c4b2702924627a8de01a3550c22038a06595
Derrick Dunville <derrickdunville@gmail.com>
2019-04-24 12:45:40 -0400

    Refactored ItemTest.

--- test/test/ItemTest.java
+++ test/test/ItemTest.java
@@ -2,6 +2,7 @@ package test;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import pillar.item.QuantifiedItem;
@@ -9,29 +10,34 @@ import pillar.item.WeightedItem;
 
 public class ItemTest {
 
+	double delta = 0.000001;
+	QuantifiedItem quantifiedItem;
+	WeightedItem weightedItem; 
+	
+	@Before
+	public void initializeTest() {
+		quantifiedItem = new QuantifiedItem("QuantifiedItem", 2.59);
+		weightedItem = new WeightedItem("Test", 2.59);
+	}
 	@Test
 	public void quantifiedItemCreatedWithNameAndPrice() {
-		QuantifiedItem testItem = new QuantifiedItem("Test", 2.59);
-		assertEquals(testItem.getName(), "Test");
-		assertEquals(testItem.getPrice(), 2.59, 2);	
+		assertEquals(quantifiedItem.getName(), "Test");
+		assertEquals(quantifiedItem.getPrice(), 2.59, delta);	
 	}
 	
 	@Test
 	public void quantifiedItemSubTotalBasedOnQuantity() {
-		QuantifiedItem testItem = new QuantifiedItem("Test", 2.59);
-		assertEquals(testItem.getSubTotal(2), 2.59*2, 2);
+		assertEquals(quantifiedItem.getSubTotal(2), 2.59*2, delta);
 	}
 	
 	@Test
 	public void weightedItemCreatedWithNameAndPrice() {
-		WeightedItem testItem = new WeightedItem("Test", 2.59);
-		assertEquals(testItem.getName(), "Test");
-		assertEquals(testItem.getPrice(), 2.59, 2);	
+		assertEquals(weightedItem.getName(), "Test");
+		assertEquals(weightedItem.getPrice(), 2.59, delta);	
 	}
 	
 	@Test
 	public void weightedItemSubTotalBasedOnWeight() {
-		WeightedItem testItem = new WeightedItem("Test", 2.59);
-		assertEquals(testItem.getSubTotal(2.00), 2.59*2.00, 2);
+		assertEquals(weightedItem.getSubTotal(2.00), 2.59*2.00, delta);
 	}
 }
